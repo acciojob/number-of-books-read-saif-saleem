c8b50123799d6b4b3a716ec0c9136929b3dee5fd
saif-saleem <133539527+saif-saleem@users.noreply.github.com>
2023-09-14 22:48:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,25 +1,38 @@
 const library = [
-  {
-    author: "Bill Gates",
-    title: "The Road Ahead",
-    readingStatus: true,
-  },
-  {
-    author: "Steve Jobs",
-    title: "Walter Isaacson",
-    readingStatus: true,
-  },
-  {
-    author: "Suzanne Collins",
-    title: "Mockingjay: The Final Book of The Hunger Games",
-    readingStatus: false,
-  },
-];
+    {
+      author: "Bill Gates",
+      title: "The Road Ahead",
+      readingStatus: true,
+    },
+    {
+      author: "Steve Jobs",
+      title: "Walter Isaacson",
+      readingStatus: true,
+    },
+    {
+      author: "Suzanne Collins",
+      title: "Mockingjay: The Final Book of The Hunger Games",
+      readingStatus: false,
+    },
+  ];
+  
+  const numberOfBooksRead = () => {
+    // write your code here
+    let c=0;
+    for(let i in library){
+        // console.log(typeof(i));
+        for(let j in library[i]){
+        if(typeof(library[i][j])==Boolean);{
+            if(library[i][j]==true)
+            c++;
+        }
 
-const numberOfBooksRead = () => {
-  // write your code here
-};
+        }
 
-// Do not change the code below
-
-alert(numberOfBooksRead());
+    }
+    return c;
+  }
+  
+  // Do not change the code below
+  
+  alert(numberOfBooksRead());
